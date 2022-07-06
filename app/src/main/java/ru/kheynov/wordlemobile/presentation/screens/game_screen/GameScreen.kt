9b85ef04b912a84eab71a4410f30e2f2a82ade06
Vladislav Kheynov <lcastory86@gmail.com>
2022-07-06 23:25:54 +0700

    feat: implement basic functionality for GameScreen

--- app/src/main/java/ru/kheynov/wordlemobile/presentation/screens/game_screen/GameScreen.kt
+++ app/src/main/java/ru/kheynov/wordlemobile/presentation/screens/game_screen/GameScreen.kt
@@ -4,30 +4,37 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import ru.kheynov.wordlemobile.presentation.screens.game_screen.components.AnswerGrid
+import ru.kheynov.wordlemobile.presentation.screens.game_screen.components.Header
 import ru.kheynov.wordlemobile.presentation.screens.game_screen.components.Keyboard
 
 @Composable
 fun GameScreen(
     viewModel: GameScreenViewModel = hiltViewModel(),
 ) {
-    val state = viewModel.state.observeAsState()
+    val state = viewModel.screenState.observeAsState()
+
+    val answerGrid = viewModel.answerState.observeAsState()
 
     Column(horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween) {
-        Text(text = state.value.toString(), fontSize = 24.sp)
+        Header(language = viewModel.language, onLanguageChange = {})
+        AnswerGrid(state = answerGrid.value)
         Box(modifier = Modifier
-            .fillMaxSize()
+            .wrapContentHeight()
             .clickable { viewModel.loadWord() },
             contentAlignment = Alignment.BottomCenter) {
-            Keyboard()
+            Keyboard(
+                onErase = { viewModel.eraseLetter() },
+                onEnter = { viewModel.checkWord() },
+                onLetterClick = { letter -> viewModel.appendLetter(letter) }
+            )
         }
     }
 }
--- app/src/main/java/ru/kheynov/wordlemobile/presentation/screens/game_screen/GameScreenViewModel.kt
+++ app/src/main/java/ru/kheynov/wordlemobile/presentation/screens/game_screen/GameScreenViewModel.kt
@@ -11,6 +11,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import ru.kheynov.wordlemobile.data.repository.WordleRepositoryImpl
+import ru.kheynov.wordlemobile.presentation.util.Cell
+import ru.kheynov.wordlemobile.presentation.util.Language
+import ru.kheynov.wordlemobile.presentation.util.LetterState
 import javax.inject.Inject
 
 
@@ -21,31 +24,83 @@ class GameScreenViewModel @Inject constructor(
     private val repository: WordleRepositoryImpl,
 ) : ViewModel() {
 
-    var state = MutableLiveData<GameScreenState>()
+    private var rowCounter = 0
+    private var columnCounter = 0
+
+    var language: String = repository.language
         private set
 
+    var screenState = MutableLiveData<GameScreenState>()
+        private set
+
+    private val _answerState: MutableList<Cell> = emptyList<Cell>().toMutableList()
+
+    var answerState = MutableLiveData<List<Cell>>()
+        private set
 
     private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
         viewModelScope.launch {
             Log.i(TAG, "Error: ${throwable.message}")
-            state.postValue(GameScreenState.Error(throwable.message.toString()))
+            screenState.postValue(GameScreenState.Error(throwable.message.toString()))
         }
     }
 
     fun loadWord() {
         CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
-            state.postValue(GameScreenState.Loading)
-            val response = repository.getWord(repository.language)
+            screenState.postValue(GameScreenState.Loading)
+            val response = repository.getWord(language)
             withContext(Dispatchers.Main) {
                 if (response.isSuccessful) {
-                    state.postValue(GameScreenState.Loaded(response.body()))
-                    Log.i(TAG, "loadWord: DATA, ${state.value.toString()}")
+                    screenState.postValue(GameScreenState.Loaded(response.body()))
+                    answerState.postValue(mutableListOf())
+                    Log.i(TAG, "loadWord: DATA, ${screenState.value.toString()}")
                     return@withContext
                 }
-                state.postValue(GameScreenState.Error(response.message()))
-                Log.i(TAG, "loadWord: Error, ${state.value}")
+                screenState.postValue(GameScreenState.Error(response.message()))
+                Log.i(TAG, "loadWord: Error, ${screenState.value}")
 
             }
         }
+    }
+
+    fun changeLanguage(language: Language) {
+        this.language = language.text
+        loadWord()
+    }
+
+    fun appendLetter(letter: Char) {
+        if (rowCounter <= 4) {
+            _answerState.add(
+                Cell(
+                    columnCounter,
+                    rowCounter,
+                    LetterState.NOT_USED,
+                    letter
+                )
+            )
+            rowCounter++
+            answerState.postValue(_answerState.toList())
+            Log.i(TAG, "state: ${answerState.value.toString()}")
+        } else {
+            Log.i(TAG, "End reached")
+        }
+    }
+
+    fun eraseLetter() {
+        if (rowCounter > 0) {
+            _answerState.removeLast()
+            rowCounter--
+            answerState.postValue(_answerState.toList())
+        } else {
+            Log.i(TAG, "eraseLetter: start reached")
+        }
+    }
+
+    fun checkWord() {
+        columnCounter++
+        rowCounter = 0
+
+        //TODO: checking word correctness
+        if (columnCounter == 5) return //TODO: end reached
     }
 }
