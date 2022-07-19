0cbdd2544fe79184e891c5e1a94affae1a0991da
Vladislav Kheynov <lcastory86@gmail.com>
2022-07-19 22:51:55 +0700

    feat: implement cell animation

--- app/src/main/java/ru/kheynov/wordlemobile/presentation/screens/game_screen/components/AnswerCell.kt
+++ app/src/main/java/ru/kheynov/wordlemobile/presentation/screens/game_screen/components/AnswerCell.kt
@@ -1,6 +1,8 @@
 package ru.kheynov.wordlemobile.presentation.screens.game_screen.components
 
 import android.content.res.Configuration
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -13,8 +15,12 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,15 +30,35 @@ import ru.kheynov.wordlemobile.presentation.theme.LightGridDividerColor
 import ru.kheynov.wordlemobile.presentation.theme.WordleMobileTheme
 import ru.kheynov.wordlemobile.presentation.util.LetterState
 
+private const val TAG = "AnswerCell"
+
 @Composable
 fun AnswerCell(
     modifier: Modifier = Modifier,
     state: LetterState,
     letter: Char?,
 ) {
+    val isScaleAnimated = letter != ' '
+
+    val isRotateAnimated = state != LetterState.NOT_USED
+
+    val rotation = remember { Animatable(0f) }
+
+    val scale = remember { Animatable(1f) }
+
+    LaunchedEffect(isScaleAnimated) {
+        scale.animateTo(if (isScaleAnimated) 1.08f else 1f, animationSpec = tween(150))
+    }
+
+    LaunchedEffect(isRotateAnimated) {
+        rotation.animateTo(if (isRotateAnimated) 180f else 0f, animationSpec = tween(1500))
+    }
+
     Box(
         modifier = modifier
             .defaultMinSize(40.dp, 40.dp)
+            .scale(scale.value)
+            .graphicsLayer(rotationX = rotation.value)
             .background(CellColors(isSystemInDarkTheme()).getColor(state)!!)
             .border(BorderStroke(
                 1.dp,
@@ -41,8 +67,16 @@ fun AnswerCell(
             ),
         contentAlignment = Alignment.Center
     ) {
-        Text(modifier = Modifier.padding(4.dp), text = letter.toString().uppercase(), fontSize = 40
-            .sp)
+        Text(modifier = Modifier
+            .padding(4.dp)
+            .graphicsLayer(rotationX = rotation.value),
+            text =
+            letter
+                .toString()
+                .uppercase(),
+            fontSize
+            = 40
+                .sp)
     }
 }
 
